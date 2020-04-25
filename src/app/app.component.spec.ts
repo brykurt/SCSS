2766527e0967807b806243ee7f38c69903f91e76
Bryon Kurt S. Segunto <bryonkurts@gmail.com>
2020-04-25 13:33:55 +0800

    UI/SCSS sample copywork

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,33 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { copyWorkcomponent } from './app.component';
 
-describe('AppComponent', () => {
+describe('copyWorkcomponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        copyWorkcomponent
       ],
     }).compileComponents();
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(copyWorkcomponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'copywork-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(copyWorkcomponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('copywork-app');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(copyWorkcomponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('copywork-app app is running!');
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,61 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { copyWorkcomponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormControl } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button'
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule,  } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+interface Dropdown {
+  value: string;
+  viewValue: string;
+}
 @NgModule({
+
+  exports: [
+    
+  ],
+
+
   declarations: [
-    AppComponent
+    copyWorkcomponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
+
+
+  
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [copyWorkcomponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+  
+  dropdown: Dropdown[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+
+}
